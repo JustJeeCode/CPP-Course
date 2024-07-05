26e1a0916106731b463f8dfd4bfcb94d203ce62c
JustJeeCode <giorpieri@gmail.com>
2024-07-05 17:33:38 +1000

    Added helloworld.cpp and variables.cpp

---
+++ variables.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+int main() {
+
+	/*
+	// int x; //declaration
+	int x = 5; //assignment
+	// int x = 5; declaration and assignment
+	int y = 6;
+	int sum = x + y;
+
+	std::cout << x << '\n';
+	std::cout << y << '\n';
+	std::cout << sum << '\n';
+	*/
+
+	//integer (whole number)
+	int age = 18;
+	int year = 2024;
+	int days = 7;
+
+	//double (number including decimal)
+	double price = 10.99;
+	double gpa = 2.5;
+	double temperature = 25.1;
+
+	// single character
+	char grade = 'A';
+	char initial = 'G';
+	char currency = '$';
+
+	// boolean (true or false)
+	bool student = false;
+	bool power = true;
+	bool forSale = false;
+
+	// string (objects that represent a sequence of text)
+	std::string name = "Giorgio";
+	std::string day = "Friday";
+	std::string food = "pizza";
+	std::string address = "123 Fake St.";
+
+	std::cout << "Hello " << name << '\n';
+	std::cout << "You are " << age << " years old.";
+
+	return 0;
+}
