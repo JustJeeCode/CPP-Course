3afc1e0cab16abeb6ed13609b6c851e48fc135be
JustJeeCode <giorpieri@gmail.com>
2024-07-05 18:05:23 +1000

    operators.cpp

---
+++ operators.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+int main() {
+	/* arithmetic operators = return the result of a specific
+	arithmetic operation (+ - * /)
+	*/
+
+	//parenthesis, multiplication and division, addition and subtraction
+
+	int students = 20;
+
+	//students = students + 1;
+	//students+=1;
+	//students++;
+
+	//students = students - 1;
+	//students-=1;
+	//students--;
+
+	//students = students * 2;
+	//students*=2;
+
+	//students = students / 2;
+	//students/=3;
+
+
+	int remainder = students % 2;
+
+	std::cout << remainder;
+
+
+
+	return 0;
+}
